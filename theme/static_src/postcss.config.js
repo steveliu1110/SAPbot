2cba9497112ee74761db020cc1477fd76063b0bb
steveliu1110 <steveliu1110@gmail.com>
2024-12-04 00:43:27 -0800

    add : initial

---
+++ theme/static_src/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {}
+  },
+  safelist: [
+    'bg-amber-500',
+    'bg-emerald-500',
+    'bg-red-500',
+  ],
+}
